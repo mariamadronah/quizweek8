8e9d7d944310b9187d19dd12f6ed255c3427f839
mariamadronah <mariamnb6@gmail.com>
2022-10-10 12:59:50 +0300

    week8quiz

---
+++ quiz8.py
@@ -0,0 +1,6 @@
+#Write a program that creates a file named answers.txt
+#and writes all the answers to the questions in the file.
+#The program should then read the file and 
+#display the contents on the screen.
+
+file = open('answers.txt','r')
